9e6c5fa5666b64ddc48f730b10179439229fd83d
MasterZeeno <zeenoliev@gmail.com>
2025-05-23 11:41:17 +0800

    Update

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 gh config set -h github.com git_protocol https
-echo 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 | gh auth login --with-token
+echo "${1:?}" | gh auth login --with-token
