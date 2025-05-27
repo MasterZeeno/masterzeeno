87c2d1df987c2c66d92ba3093f97a829c0960bfe
MasterZeeno <zeenoliev@gmail.com>
2025-05-27 18:42:17 +0800

    Update

--- scripts/test.zsh
+++ scripts/test.zsh
@@ -1,42 +1,47 @@
 #!/data/data/com.termux/files/usr/bin/zsh
 
 
-test() {
-    local var='__p9k_colors'
-    local file="./cache/$var"
-    local repo_owner="romkatv"
-    local repo_name="powerlevel10k"
-    local file_path="internal/p10k.zsh"
+# test() {
+    # local var='__p9k_colors'
+    # local file="./cache/$var"
+    # local repo_owner="romkatv"
+    # local repo_name="powerlevel10k"
+    # local file_path="internal/p10k.zsh"
     
-    [[ -s "$file" ]] || echo '# v0.0.0' > "$file"
-    local current_version=$(head -n1 "$file")
+    # [[ -s "$file" ]] || echo '# v0.0.0' > "$file"
+    # local current_version=$(head -n1 "$file")
     
-        local contents=(
-            "$(get_latest_release "$repo_owner" "$repo_name")"
-            "$(view_file_online "$repo_owner" "$repo_name" "$file_path" | \
-                sed -n "/^typeset.*$var=(/,/)/p")")
+        # local contents=(
+            # "$(get_latest_release "$repo_owner" "$repo_name")"
+            # "$(view_file_online "$repo_owner" "$repo_name" "$file_path" | \
+                # sed -n "/^typeset.*$var=(/,/)/p")")
             
             
-        fi
-        local latest_release="$(get_latest_release "$repo_owner" "$repo_name")"
-        source "$file"
-    fi
-    echo "${(P)var}"
-}
+        # fi
+        # local latest_release="$(get_latest_release "$repo_owner" "$repo_name")"
+        # source "$file"
+    # fi
+    # echo "${(P)var}"
+# }
 
-get_latest_release() { 
-    local repo_owner="$1"
-    local repo_name="$2"
-    curl -s "https://api.github.com/repos/$repo_owner/$repo_name/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
-}
+# get_latest_release() { 
+    # local repo_owner="$1"
+    # local repo_name="$2"
+    # curl -s "https://api.github.com/repos/$repo_owner/$repo_name/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+# }
 
-view_file_online() {
-    local repo_owner="$1"
-    local repo_name="$2"
-    local file_path="$3"
-    curl -s "https://raw.githubusercontent.com/$repo_owner/$repo_name/refs/heads/master/$file_path"
-}
+# view_file_online() {
+    # local repo_owner="$1"
+    # local repo_name="$2"
+    # local file_path="$3"
+    # curl -s "https://raw.githubusercontent.com/$repo_owner/$repo_name/refs/heads/master/$file_path"
+# }
 
-# get_latest_release "romkatv" "powerlevel10k"
+# # get_latest_release "romkatv" "powerlevel10k"
 
-test
+# test
+
+
+url='https://github.com/Zackptg5/Cross-Compiled-Binaries-Android/blob/master/zsh/etc.zip'
+aria2c -x 16 -s 64 -j 1 \
+  --max-tries=3 --retry-wait=2 -d "." "$url"
